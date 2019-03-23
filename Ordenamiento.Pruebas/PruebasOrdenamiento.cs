2a779b2a7623d25d422f2b1188b02a3c9f4504b6
Your Name <jestrada14@ucol.com>
2019-03-23 00:08:42 -0600

    Paso 5, Ordenamiento no arrgelado

--- Ordenamiento.Pruebas/PruebasOrdenamiento.cs
+++ Ordenamiento.Pruebas/PruebasOrdenamiento.cs
@@ -8,11 +8,37 @@ namespace Ordenamiento.Pruebas
         private int[] salidaEsperada = new int[]{1, 2, 3, 4, 5};
         private Ordenamiento.Burbuja ordenar = new Ordenamiento.Burbuja();
         [TestMethod]
-        public void Ordenar(){
+        public void Ordenar1(){
             int[] entrada = new int[]{1, 2, 3, 4, 5};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }       
+        [TestMethod]
+        public void Ordenar2(){
+            int[] entrada = new int[]{1, 3, 2, 4, 5};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
 
+        [TestMethod]
+        public void Ordenar3(){
+            int[] entrada = new int[]{5, 1, 2, 3, 4};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void Ordenar4(){
+            int[] entrada = new int[]{2, 3, 4, 5, 1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void Ordenar5(){
+            int[] entrada = new int[]{5, 4, 3, 2, 1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
         }
     }
 }
