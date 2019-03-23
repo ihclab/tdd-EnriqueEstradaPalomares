a19db589b0a63a13202816a42fce6a21e65b1fd1
Your Name <jestrada14@ucol.com>
2019-03-23 00:14:53 -0600

    COMPLETADO

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -6,11 +6,14 @@ namespace Ordenamiento
     {
         public void Ordenar(int[] arr){
             for (int i = 0; i < arr.Length - 1; i++){
-                if(arr[i] > arr[i + 1]){
-                    int aux = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = aux;
+                for (int j = 0; j < arr.Length - 1; j++){
+                    if(arr[j] > arr[j + 1]){
+                    int aux = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = aux;
+                    }
                 }
+                //en el anterior, solo recorria una vez, por eso se agrega el for de "j", este realiza el cambio, mientras que "i" continua sus iteraciones
             }
         }
     }
